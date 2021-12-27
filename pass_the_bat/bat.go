ef1c05ab3950f5e5efa476ec6ac755bebd61c885
Ruan Montelo <ruanmontelo@gmail.com>
2021-12-27 14:31:51 -0300

    Returning the next runner's index from the channel as an example of something that can be done in a pipeline.

--- pass_the_bat/bat.go
+++ pass_the_bat/bat.go
@@ -8,8 +8,6 @@ import (
 	"github.com/JustAn0therDev/go_concurrency/go_concurrency_util"
 )
 
-// const runningManRune rune = '🏃'
-
 // TODO: For now, there will only be a single line of men running and passing the batons.
 // TODO: make a thread pass the bat to each running man with channels,
 // but make it a race.
@@ -27,15 +25,16 @@ func StartPassTheBat() {
 }
 
 func UpdateRunningMen(runningMen []string, done *bool) {
-	passingBat := make(chan bool)
-	for i := len(runningMen) - 1; i >= 0; i-- {
-		go updateRunningManString(&runningMen[i], passingBat)
-		<-passingBat
+	passingBatTo := make(chan int, 1)
+	var idx int = len(runningMen) - 1
+	for idx >= 0 {
+		go updateRunningManString(&runningMen[idx], idx, passingBatTo)
+		idx = <-passingBatTo
 	}
 	*done = true
 }
 
-func updateRunningManString(runningMan *string, passingBat chan bool) {
+func updateRunningManString(runningMan *string, idx int, passingBatTo chan int) {
 	stringSize := len([]rune(*runningMan)) - 1
 	for i := stringSize; i > 0; i-- {
 		time.Sleep(time.Second / 2)
@@ -43,6 +42,5 @@ func updateRunningManString(runningMan *string, passingBat chan bool) {
 		rmRuneSlice[i], rmRuneSlice[i - 1] = rmRuneSlice[i - 1], rmRuneSlice[i]
 		*runningMan = string(rmRuneSlice)
 	}
-
-	passingBat<-true
+	passingBatTo <- idx - 1
 }
